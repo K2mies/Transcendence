3989524470e5949aae8d4cabf209b2c48a333064
Viljar Uljas <viljar1990@gmail.com>
2026-04-28 10:16:43 +0300

    GG04: setup: backend base setup + signal handler

---
+++ backend/src/controllers/health.controllers.js
@@ -0,0 +1,31 @@
+export const healthCheck = async (req, res) => {
+	try {
+		// DB healhtcheck, replace later  with real, simulation atm
+		const dbHealth = await Promise.resolve(true);
+		if (!dbHealth) {
+			return res.status(503).json({
+				status: "ERROR",
+				dependencies: {
+					database: "DOWN"
+				},
+				timestamp: new Date().toISOString(),
+				message: "Dependency failure"
+			});
+		}
+		return res.status(200).json({
+			status: "OK",
+			uptime: process.uptime(),
+			dependencies: {
+				database: "UP"
+			},
+			timestamp: new Date().toISOString(),
+			message: "Server is running"
+		});
+	} catch (error) {
+		return res.status(500).json({
+			status: "ERROR",
+			timestamp: new Date().toISOString(),
+			message: "Health check failed"
+		});
+	}
+};
