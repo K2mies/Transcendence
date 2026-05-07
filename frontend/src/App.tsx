2b309ca5e8d4211df166902392286e79c22c3dd1
Ross Hvidsten <Rosshvidsten@googlemail.com>
2026-05-07 13:38:38 +0300

    added temporary logo to splash page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,60 +1,57 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import './App.css'
-import MyForm from "./Form.jsx"
-import isSigned from "./IsSigned.ts"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import MyForm from "./Form.jsx";
+
+let signed: boolean = false;
 
 function Header() {
-	return (
-		<div>
-			<nav className="header">
-				<Link to="/">GoodPlays</Link>
-				<Link to="/explore">Explore</Link>
-				<Link to="/mygames">My games</Link>
-			</nav>
-		</div>
-	)
+  return (
+    <div>
+      <nav className="header">
+        <Link to="/">GoodPlays</Link>
+        <Link to="/explore">Explore</Link>
+        <Link to="/mygames">My games</Link>
+      </nav>
+    </div>
+  );
 }
 
 function Home() {
-	if (!isSigned.value) {
-		return (
-			<div>
-				<h1>GoodPlays</h1>
-				<h2>Welcome to GoodPlays!</h2>
-				<p>Already have an account?</p>
-				<p>New user?</p>
-				<Link to="/register">Sign up!</Link>
-			</div>
-		)
-	} else {
-		return (
-			<Header></Header>
-			/* Here we should have the home page displayed after signing in.
-				What is displayed, how does it differ from Explore? */
-		)
-	}
+  if (!signed) {
+    return (
+      <div>
+        <img src="/logo_03.jpg" alt="GoodPlays logo" />
+        <h1>GoodPlays</h1>
+        <h2>Welcome to GoodPlays!</h2>
+        <p>Already have an account?</p>
+        <p>New user?</p>
+        <Link to="/register">Sign up!</Link>
+      </div>
+    );
+  }
 }
 
 function SignUp() {
-	return (
-		<div>
-			<h2>Sign up to GoodPlays</h2>
-			<MyForm></MyForm>
-		</div>
-	)
+  return (
+    <div>
+      <h2>Sign up to GoodPlays</h2>
+      <MyForm>FORM</MyForm>
+    </div>
+  );
 }
 
 function App() {
   return (
     <BrowserRouter>
-		<Routes>
-			<Route path="/" element={<Home />} />
-			{/* <Route path="/explore" element={<Explore />} />
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/explore" element={<Explore />} />
 			<Route path="/mygames" element={<MyGames />} /> */}
-			<Route path="/register" element={<SignUp />} />
-		</Routes>
+        <Route path="/register" element={<SignUp />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
