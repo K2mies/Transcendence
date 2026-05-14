ac307e40c0f29add1e629569c66b40a69451b226
Viljar Uljas <viljar1990@gmail.com>
2026-05-14 13:05:46 +0300

    GG93: fix: migrations

--- backend/prisma/migrations/20260513130102_make_platform_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "UserGameRelation" DROP CONSTRAINT "UserGameRelation_platformId_fkey";
-
--- AlterTable
-ALTER TABLE "UserGameRelation" ALTER COLUMN "platformId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "UserGameRelation" ADD CONSTRAINT "UserGameRelation_platformId_fkey" FOREIGN KEY ("platformId") REFERENCES "Platform"("id") ON DELETE SET NULL ON UPDATE CASCADE;
