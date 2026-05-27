71d8012fc1ccb6e79ff36e74bfcb41af4757bd93
sonjasonjao <182800674+sonjasonjao@users.noreply.github.com>
2026-05-27 09:50:41 +0300

    GG23: fix: removing isSigned logic now that we have route protection coming in a pull request soon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,6 @@ import "./App.css";
 import RegisterForm from "./RegisterForm";
 import LoginForm from "./LoginForm";
 import DisplayProfile from "./Profile"
-import isSigned from "./isSigned"
 
 function Header() {
   return (
@@ -24,27 +23,23 @@ function Header() {
 }
 
 function Home() {
-  if (!isSigned.value) {
-    return (
-      <div style={{ margin: "0px" }}>
-        <img src="/logo_03.jpg" alt="GoodPlays logo" />
-
-        <h1>GoodPlays</h1>
-        <h2>Welcome to GoodPlays!</h2>
-		<div>
-			<p>Already have an account?</p>
-			<Link to="/login">Log in</Link>
-		</div>
-		<div>
-			<p>New user?</p>
-			<Link to="/register">Sign up!</Link>
-		</div>
-		<Link to="/user/xKr4t0sx">Test profile display (user xKr4t0sx)</Link>
+  return (
+    <div style={{ margin: "0px" }}>
+      <img src="/logo_03.jpg" alt="GoodPlays logo" />
+      <h1>GoodPlays</h1>
+      <h2>Welcome to GoodPlays!</h2>
+        <div>
+          <p>Already have an account?</p>
+          <Link to="/login">Log in</Link>
+        </div>
+        <div>
+          <p>New user?</p>
+          <Link to="/register">Sign up!</Link>
+        </div>
+        <Link to="/user/xKr4t0sx">Test profile display (user xKr4t0sx)</Link>
       </div>
     );
   }
-  return <div>Signed in homepage</div>;
-}
 
 function SignUp() {
   return (
