2b6f34dddd185a573f13e097db0bd91d8f11ad2a
sonjasonjao <182800674+sonjasonjao@users.noreply.github.com>
2026-05-27 15:00:13 +0300

    GG34: Including reviews, modes, genres, and platforms to game object for game page UI. Adding frontend volumes to docker compose to make testing UI dynamic. Progressing with building game page UI. Changing tabs to spaces in edited files

--- backend/src/services/game.service.js
+++ backend/src/services/game.service.js
@@ -4,6 +4,43 @@ export async function getGame(gameName)
 {
 	const game = await prisma.game.findUnique({
 	where: { name: gameName },
+		select: {
+			id: true,
+			name: true,
+			imageBig: true,
+			description: true,
+			releaseDate: true,
+			updateDate: true,
+			developer: true,
+			publisher: true,
+			rating: true,
+			reviews: {
+				select: {
+					id: true,
+					userId: true,
+					review: true,
+					rating: true,
+				}
+			},
+			modes: {
+				select: {
+					id: true,
+					name: true,
+				}
+			},
+			genres: {
+				select: {
+					id: true,
+					name: true,
+				}
+			},
+			platforms: {
+				select: {
+					id: true,
+					name: true,
+				}
+			},
+		}
 	});
 	return {
 		id: game.id,
@@ -15,5 +52,9 @@ export async function getGame(gameName)
 		developer: game.developer,
 		publisher: game.publisher,
 		rating: game.rating,
+		reviews: game.reviews,
+		modes: game.modes,
+		genres: game.genres,
+		platforms: game.platforms
 	}
 }
--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -4,145 +4,163 @@ import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { useSwiper } from "swiper/react";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/navigation";
 
 function ProfileInfo(props) {
-	return (
-		<div className="user-info">
-			<div className="user-header">
-				<h2>{props.profile.name}</h2>
-				<button>Add friend</button>
-			</div>
-			<div className="user-content">
-				<img className="user-img" src="/logo_03.jpg" alt="Placeholder for profile picture"></img>
-				<p className="user-bio">{props.profile.bio}</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="user-game-info">
+      <div className="user-game-header">
+        <h2>{props.profile.name}</h2>
+        <button>Add friend</button>
+      </div>
+      <div className="user-game-content">
+        <img
+          className="user-img"
+          src="/logo_03.jpg"
+          alt="Placeholder for profile picture"
+        ></img>
+        <p className="user-game-bio">{props.profile.bio}</p>
+      </div>
+    </div>
+  );
 }
 
 const SwiperButtonPrev = ({ children }) => {
-	const swiper = useSwiper();
-	return (
-		<div className="swiper-button-prev" onClick={() => swiper.slidePrev()}>{children}</div>
-	);
-}
+  const swiper = useSwiper();
+  return (
+    <div className="swiper-button-prev" onClick={() => swiper.slidePrev()}>
+      {children}
+    </div>
+  );
+};
 
 const SwiperButtonNext = ({ children }) => {
-	const swiper = useSwiper();
-	return (
-		<div className="swiper-button-next" onClick={() => swiper.slideNext()}>{children}</div>
-	);
-}
+  const swiper = useSwiper();
+  return (
+    <div className="swiper-button-next" onClick={() => swiper.slideNext()}>
+      {children}
+    </div>
+  );
+};
 
 function DisplayGames(props) {
-	return (
-		<>
-		<h4 className="user-games-header">{props.header}</h4>
-		{props.games.length > 5 ? (
-			<div>
-				<Swiper
-					style={{ '--swiper-navigation-color': 'red' }}
-					modules={Navigation}
-					spaceBetween={30}
-					slidesPerView={5}
-					navigation={{
-						prevEl: ".swiper-button-prev",
-						nextEl: ".swiper-button-next",
-					}}
-					allowTouchMove={false}
-					>
-					{props.games.map((game, index) => (
-						<SwiperSlide key={game.id}>
-						<img className="game-card-img" src={game.image} alt={game.name}></img>
-						<p>{game.name}</p>
-					</SwiperSlide>
-					))}
-					<SwiperButtonPrev>&lt;</SwiperButtonPrev>
-					<SwiperButtonNext>&gt;</SwiperButtonNext>
-				</Swiper>
-			</div>
-		) : (
-			<div className="user-games">
-				<ul className="game-cards">
-					{props.games.map((game) => (
-						<li key={game.id} className="game-card">
-							<img className="game-card-img" src={game.image} alt={game.name}></img>
-							<p>{game.name}</p>
-						</li>
-					))}
-				</ul>
-			</div>
-		)}
-		</>
-	)
+  return (
+    <>
+      <h4 className="user-game-header">{props.header}</h4>
+      {props.games.length > 5 ? (
+        <div>
+          <Swiper
+            style={{ "--swiper-navigation-color": "red" }}
+            modules={Navigation}
+            spaceBetween={30}
+            slidesPerView={5}
+            navigation={{
+              prevEl: ".swiper-button-prev",
+              nextEl: ".swiper-button-next",
+            }}
+            allowTouchMove={false}
+          >
+            {props.games.map((game, index) => (
+              <SwiperSlide key={game.id}>
+                <img
+                  className="game-card-img"
+                  src={game.image}
+                  alt={game.name}
+                ></img>
+                <p>{game.name}</p>
+              </SwiperSlide>
+            ))}
+            <SwiperButtonPrev>&lt;</SwiperButtonPrev>
+            <SwiperButtonNext>&gt;</SwiperButtonNext>
+          </Swiper>
+        </div>
+      ) : (
+        <div className="user-games">
+          <ul className="game-cards">
+            {props.games.map((game) => (
+              <li key={game.id} className="game-card">
+                <img
+                  className="game-card-img"
+                  src={game.image}
+                  alt={game.name}
+                ></img>
+                <p>{game.name}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
 
 function DisplayProfile() {
-	const [profile, setProfile] = useState({});
-	const [favGames, setFavGames] = useState([]);
-	const [currGames, setCurrGames] = useState([]);
-	const [toPlayGames, setToPlayGames] = useState([]);
-	const [completedGames, setCompletedGames] = useState([]);
-	const [isUserFound, setIsUserFound] = useState(undefined);
-	const { username } = useParams();
+  const [profile, setProfile] = useState({});
+  const [favGames, setFavGames] = useState([]);
+  const [currGames, setCurrGames] = useState([]);
+  const [toPlayGames, setToPlayGames] = useState([]);
+  const [completedGames, setCompletedGames] = useState([]);
+  const [isUserFound, setIsUserFound] = useState(undefined);
+  const { username } = useParams();
 
-	useEffect(() => {
-		async function loadProfile() {
-			const response = await fetch(`http://localhost:4243/profile/${user}`);
-			if (response.status === 200) {
-				const res = await response.json();
-				setIsUserFound(true);
-				setProfile(res);
-				if (res.favorites.length > 0)
-					setFavGames(res.favorites);
-				if (res.playing.length > 0)
-					setCurrGames(res.playing);
-				if (res.to_play.length > 0)
-					setToPlayGames(res.to_play);
-				if (res.completed.length > 0)
-					setCompletedGames(res.completed);
-			} else {
-				setIsUserFound(false);
-			}
-		}
+  useEffect(() => {
+    async function loadProfile() {
+      const response = await fetch(`http://localhost:4243/profile/${username}`);
+      if (response.status === 200) {
+        const res = await response.json();
+        setIsUserFound(true);
+        setProfile(res);
+        if (res.favorites.length > 0) setFavGames(res.favorites);
+        if (res.playing.length > 0) setCurrGames(res.playing);
+        if (res.to_play.length > 0) setToPlayGames(res.to_play);
+        if (res.completed.length > 0) setCompletedGames(res.completed);
+      } else {
+        setIsUserFound(false);
+      }
+    }
 
-		loadProfile();
-	}, []);
+    loadProfile();
+  }, []);
 
-	return (
-		<div>
-			{isUserFound &&
-			<div>
-				<ProfileInfo profile={profile}></ProfileInfo>
-				{favGames.length > 0 &&
-				<DisplayGames
-					header="Favorite games"
-					games={favGames}>
-				</DisplayGames>}
-				{currGames.length > 0 &&
-				<DisplayGames
-					header="Currently playing"
-					games={currGames}>
-				</DisplayGames>}
-				{toPlayGames.length > 0 &&
-				<DisplayGames
-					header="Games to play"
-					games={toPlayGames}>
-				</DisplayGames>}
-				{completedGames.length > 0 &&
-				<DisplayGames
-					header="Completed games"
-					games={completedGames}>
-				</DisplayGames>}
-			</div>}
-			{isUserFound === false &&
-			<div>
-				<p>404 User not found</p>
-			</div>}
-		</div>
-	);
+  return (
+    <div>
+      {isUserFound && (
+        <div>
+          <ProfileInfo profile={profile}></ProfileInfo>
+          {favGames.length > 0 && (
+            <DisplayGames
+              header="Favorite games"
+              games={favGames}
+            ></DisplayGames>
+          )}
+          {currGames.length > 0 && (
+            <DisplayGames
+              header="Currently playing"
+              games={currGames}
+            ></DisplayGames>
+          )}
+          {toPlayGames.length > 0 && (
+            <DisplayGames
+              header="Games to play"
+              games={toPlayGames}
+            ></DisplayGames>
+          )}
+          {completedGames.length > 0 && (
+            <DisplayGames
+              header="Completed games"
+              games={completedGames}
+            ></DisplayGames>
+          )}
+        </div>
+      )}
+      {isUserFound === false && (
+        <div>
+          <p>404 User not found</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default DisplayProfile;
