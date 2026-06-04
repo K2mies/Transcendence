337a1f8882095707ea900e776da92171620c301b
Ross Hvidsten <Rosshvidsten@googlemail.com>
2026-06-04 13:57:55 +0300

    updated the tailwind formatting of components (register and login and home), took out the profile test link on home as well

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,25 +9,24 @@ import RegisterForm from "./RegisterForm";
 import LoginForm from "./LoginForm";
 import ProtectedRoute from "./ProtectedRoute";
 import PublicRoute from "./PublicRoute";
-import DisplayProfile from "./Profile";
+import Profile from "./Profile";
 import Dashboard from "./Dashboard";
 import Header from "./Header";
 import Game from "./Game";
 
 function Home() {
   return (
-    <div className="m-0 flex flex-col">
-      <img
-        className="w-126.25 h-107.75"
-        src="/logo_03.jpg"
-        alt="GoodPlays logo"
-      />
+    <div className="m-0 flex flex-col items-center gap-4 text-center">
+      <img className="w-96 h-auto" src="/logo_03.jpg" alt="GoodPlays logo" />
+
       <h1>GoodPlays</h1>
       <h2>Welcome to GoodPlays!</h2>
+
       <div>
         <p>Already have an account?</p>
         <Link to="/login">Log in</Link>
       </div>
+
       <div>
         <p>New user?</p>
         <Link to="/register">Sign up!</Link>
@@ -38,7 +37,7 @@ function Home() {
 
 function SignUp() {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center justify-center text-center">
       <h2>Sign up to GoodPlays</h2>
       <RegisterForm></RegisterForm>
       <p>Already have an account?</p>
@@ -49,19 +48,11 @@ function SignUp() {
 
 function Login() {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center justify-center text-center">
       <h2>Login to Goodplays</h2>
       <LoginForm></LoginForm>
       <p>New user?</p>
       <Link to="/register">Sign up</Link>
-    </div>
-  );
-}
-
-function Profile() {
-  return (
-    <div>
-      <DisplayProfile></DisplayProfile>
     </div>
   );
 }
@@ -83,7 +74,7 @@ function Layout() {
 
         <Route element={<ProtectedRoute />}>
           <Route path="dashboard" element={<Dashboard />} />
-          <Route path="user/:username" element={<DisplayProfile />} />
+          <Route path="user/:username" element={<Profile />} />
           <Route path="game/:name" element={<Game />} />
         </Route>
       </Routes>
