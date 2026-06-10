434f4919b811cd7e9100d59c83fc96b77b9449af
Ross Hvidsten <Rosshvidsten@googlemail.com>
2026-06-10 17:01:35 +0300

    added stars system to it's own component and then also ratings on the Games page

--- frontend/src/Games.tsx
+++ frontend/src/Games.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Stars from "./Stars";
 
 type Game = {
   id: number;
   name: string;
   imageSmall: string;
+  imageBig: string;
   developer: string | null;
+  rating: number;
 };
 
 type Pagination = {
@@ -41,13 +45,30 @@ function Games() {
     <div className="bg-secondary text-primary min-h-screen p-6">
       <h1>Games</h1>
 
-      {games.map((game) => (
-        <div key={game.id}>
-          <img src={game.imageBig} alt={game.name} />
-          <h2>{game.name}</h2>
-          <p>{game.developer}</p>
-        </div>
-      ))}
+      <div className="grid grid-cols-4 gap-6">
+        {games.map((game) => (
+          <div key={game.id} className="w-[234px]">
+            <Link to={"/game/" + game.name}>
+              <img
+                src={game.imageBig}
+                alt={game.name}
+                className="border-primary border-3 w-full rounded-t-lg"
+              />
+            </Link>
+
+            <div className=" bg-tertiary text-primary border-primary border-3 text-center rounded-b-lg">
+              <h2 className=" bg-primary text-tertiary break-words p-2 text-[90%]">
+                <Link to={"/game/" + game.name} className="no-underline">
+                  {game.name}
+                </Link>
+              </h2>
+              <p className="p-2">{game.developer}</p>
+              <p> Rating: {game.rating}</p>
+              <Stars rating={game.rating} />
+            </div>
+          </div>
+        ))}
+      </div>
 
       <div>
         <button
