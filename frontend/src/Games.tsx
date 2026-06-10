ba3ccb578c002e7d205de7fc1cbafb11937cbc59
Ross Hvidsten <Rosshvidsten@googlemail.com>
2026-06-10 15:32:59 +0300

    added the games page front end with pagination and now trying to solve grid issues

--- frontend/src/Games.tsx
+++ frontend/src/Games.tsx
@@ -1,7 +1,75 @@
+import { useEffect, useState } from "react";
+
+type Game = {
+  id: number;
+  name: string;
+  imageSmall: string;
+  developer: string | null;
+};
+
+type Pagination = {
+  page: number;
+  limit: number;
+  totalItems: number;
+  totalPages: number;
+};
+
 function Games() {
+  const [games, setGames] = useState<Game[]>([]);
+  const [pagination, setPagination] = useState<Pagination | null>(null);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    async function fetchGames() {
+      const response = await fetch(
+        `http://localhost:4243/search?page=${page}`,
+        {
+          credentials: "include",
+        },
+      );
+
+      const result = await response.json();
+
+      if (result.status === "success") {
+        setGames(result.data);
+        setPagination(result.pagination);
+      }
+    }
+    fetchGames();
+  }, [page]);
   return (
-    <div className="bg-secondary text-primary min-h-screen">
-      <h2>Games</h2>
+    <div className="bg-secondary text-primary min-h-screen p-6">
+      <h1>Games</h1>
+
+      {games.map((game) => (
+        <div key={game.id}>
+          <img src={game.imageBig} alt={game.name} />
+          <h2>{game.name}</h2>
+          <p>{game.developer}</p>
+        </div>
+      ))}
+
+      <div>
+        <button
+          disabled={page === 1}
+          onClick={() => setPage((prev) => prev - 1)}
+        >
+          Previous
+        </button>
+        <span>
+          Page {pagination?.page} of {pagination?.totalPages}
+        </span>
+
+        <button
+          disabled={page === pagination?.totalPages}
+          onClick={() => {
+            console.log("Next clicked");
+            setPage((prev) => prev + 1);
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
