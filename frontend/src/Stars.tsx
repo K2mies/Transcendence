511579e159131ca7bc8c165906f10e7d010004d4
Ross Hvidsten <Rosshvidsten@googlemail.com>
2026-06-11 15:38:42 +0300

    moved things into their own components (GameCard, Pagination, Star etc) made the header sticky and the pagination controlls in games stick, solved the title problem for the cards and now they appear on hover over and solved the stars issue by using react icons svg stars instead

--- frontend/src/Stars.tsx
+++ frontend/src/Stars.tsx
@@ -1,27 +1,25 @@
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+import { PiStar, PiStarFill, PiStarHalfFill } from "react-icons/pi";
+
 type StarsProps = {
   rating: number;
 };
 
 function Stars({ rating }: StarsProps) {
   return (
-    <div className="flex justify-center mb-2">
+    <div className="flex justify-center gap-1 mb-2">
       {Array.from({ length: 5 }).map((_, index) => {
-        let star = "/star_empty.png";
-
         if (rating >= index + 1) {
-          star = "/star_full.png";
-        } else if (rating >= index + 0.5) {
-          star = "/star_half.png";
+          return <PiStarFill key={index} className="text-secondary text-xl" />;
         }
 
-        return (
-          <img
-            key={index}
-            src={star}
-            alt="star rating"
-            className="w-8 h-auto"
-          />
-        );
+        if (rating >= index + 0.5) {
+          return (
+            <PiStarHalfFill key={index} className="text-secondary text-xl" />
+          );
+        }
+
+        return <PiStar key={index} className="text-secondary text-xl" />;
       })}
     </div>
   );
