bffcbe94786d8f6e63667ac1e9454e45db7a9280
Ross Hvidsten <Rosshvidsten@googlemail.com>
2026-06-11 20:31:22 +0300

    updated header layout to make more sense and now has contextual title finding for both profiles and games

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -2,39 +2,53 @@ import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import UseChat from "./chat/UseChat";
 
-function Header()
-  {
-    const { conversations } = UseChat();
-    const hasUnreadMessages = conversations.some((c) => c.unreadCount > 0);
+function Header() {
+  const { conversations } = UseChat();
+  const hasUnreadMessages = conversations.some((c) => c.unreadCount > 0);
 
-    return (
-      <nav className="flex w-full flex-row items-center gap-4 py-2 px-4 bg-[var(--color-primary)] text-[var(--color-tertiary)]">
+  let pageTitle: string;
+
+  if (location.pathname.startsWith("/user/")) {
+    pageTitle = decodeURIComponent(location.pathname.replace("/user/", ""));
+  } else if (location.pathname.startsWith("/game/")) {
+    pageTitle = decodeURIComponent(location.pathname.replace("/game/", ""));
+  } else {
+    pageTitle = pageTitles[location.pathname] || "GoodPlays";
+  }
+  return (
+    <nav className="bg-primary text-tertiary flex w-full flex-row items-center gap-6 py-2 px-4 sticky top-0 z-50">
+      <h1 className="text-tertiary">{pageTitle}</h1>
+
+      <div className="flex items-center ml-auto">
+        <button onClick={() => setShowSearch(!showSearch)}>
+          <FaSearch size={18} className="-mr-2" />
+        </button>
+
+        {showSearch && (
+          <div className=" w-96 ml-5">
+            <SearchBar />
+          </div>
+        )}
+      </div>
+      <div className="flex items-center gap-6 mr-5">
         <Link
           to="/"
           className="
-            no-underline
-            px-2
-            rounded-md
-            text-[var(--color-tertiary)]
-            bg-[var(--color-primary)]
-            transition-colors
-          "
+           no-underline 
+           text-tertiary
+           "
         >
-          GoodPlays
+          <FaHome className="text-tertiary" size={16} />
         </Link>
 
         <Link
           to="/user/xKr4t0sx"
           className="
-            no-underline
-            px-2
-            rounded-md
-            text-[var(--color-tertiary)]
-            bg-[var(--color-primary)]
-            transition-colors
-          "
+           no-underline
+           text-tertiary
+           "
         >
-          Profile
+          <FaUser className="text-tertiary" size={15} />
         </Link>
 
         <Link
@@ -48,24 +62,31 @@ function Header()
             transition-colors
           "
         >
-          Chat
+        Chat
 
-          {hasUnreadMessages && (
-            <span className="
-                              flex
-                              -top-1
-                              -right-s
-                              h-3 w-3
-                              rounded-full
-                              bg-[var(--color-online)]
-                              animate-pulse"
-            />
-          )}
+        {hasUnreadMessages && (
+          <span className="
+                            flex
+                            -top-1
+                            -right-s
+                            h-3 w-3
+                            rounded-full
+                            bg-[var(--color-online)]
+                            animate-pulse"
+          />
+        )}
+
+        <Link
+          to="/games"
+          className="
+           no-underline
+           text-tertiary
+           "
+        >
+          <FaGamepad className="text-tertiary" size={18} />
         </Link>
-        <div className="ml-auto w-[400px] shrink-0">
-          <SearchBar />
-        </div>
-      </nav>
-    );
+      </div>
+    </nav>
+  );
 }
 export default Header;
