87c4b1e4c7cf0c12a4c6e1384e30557fa0cf8d8d
Ross Hvidsten <Rosshvidsten@googlemail.com>
2026-06-12 15:02:17 +0300

    added logic for genre selection in the front end and also getting all genre's on the backend now the tags are working but i think it will need to be broken down into smaller components

--- backend/src/routes/search.routes.js
+++ backend/src/routes/search.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getGames } from "../controllers/search.controller.js";
+import { getGames, getGenres } from "../controllers/search.controller.js";
 
 const router = express.Router();
 
 router.get("/", getGames);
+router.get("/genres", getGenres); //temp delete used to get genre list
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,22 @@
 import "./config/env.js";
 import app from "./app.js";
-import {portCheck} from "./utils/portCheck.js";
-import {shutdown} from "./utils/shutdown.js";
-import {connectDB, disconnectDB} from "./config/db.js";
-import {setupWebSocket} from "./websocket/websocket.server.js";
+import { portCheck } from "./utils/portCheck.js";
+import { shutdown } from "./utils/shutdown.js";
+import { connectDB, disconnectDB } from "./config/db.js";
+import { setupWebSocket } from "./websocket/websocket.server.js";
 import http from "http";
 
-const isShuttingDown = {value: false};
+const isShuttingDown = { value: false };
 
 const BACK_PORT = process.env.BACK_PORT;
 
 let backPort;
 
 try {
-	backPort = portCheck(BACK_PORT);
+  backPort = portCheck(BACK_PORT);
 } catch (error) {
-	console.error(`${error}`);
-	process.exit(1);
+  console.error(`${error}`);
+  process.exit(1);
 }
 
 await connectDB();
@@ -27,7 +27,7 @@ setupWebSocket(server);
 
 // Open port that is defined in .env and the callback function to indicate that the PORT is listening
 server.listen(backPort, () => {
-	console.log(`Server running on ${BACK_PORT}`);
+  console.log(`Server running on ${BACK_PORT}`);
 });
 
 // Signal handlers
@@ -36,26 +36,33 @@ process.on("SIGTERM", () => shutdown("SIGTERM", isShuttingDown, server));
 
 // Handle unhandled promises rejections (e.g., database connection errors)
 process.on("unhandledRejection", (err) => {
-	console.error("Unhandled Rejection:", err);
-	server.close(async () => {
-		try {
-			await disconnectDB();
-		} catch (error) {
-			console.error("Error disconnecting DB during unhandled rejection shutdown:", error);
-		} finally {
-			process.exit(1);
-		}
-	});
+  console.error("Unhandled Rejection:", err);
+  server.close(async () => {
+    try {
+      await disconnectDB();
+    } catch (error) {
+      console.error(
+        "Error disconnecting DB during unhandled rejection shutdown:",
+        error,
+      );
+    } finally {
+      process.exit(1);
+    }
+  });
 });
 
 // Handle uncaught exceptions
 process.on("uncaughtException", async (err) => {
-	console.error("Uncaught Exception:", err);
-	try {
-		await disconnectDB();
-	} catch (error) {
-		console.error("Error disconnecting DB during uncaught exception shutdown:", error);
-	} finally {
-		process.exit(1);
-	}
+  console.error("Uncaught Exception:", err);
+  try {
+    await disconnectDB();
+  } catch (error) {
+    console.error(
+      "Error disconnecting DB during uncaught exception shutdown:",
+      error,
+    );
+  } finally {
+    process.exit(1);
+  }
 });
+
