4430c9836139bc2af8469b41ed6ed43594f11f83
sonjasonjao <182800674+sonjasonjao@users.noreply.github.com>
2026-07-03 17:39:30 +0300

    GG38: Last typo fixes

--- frontend/src/Footer/Routes/Accessibility.tsx
+++ frontend/src/Footer/Routes/Accessibility.tsx
@@ -7,12 +7,12 @@ function Accessibility() {
     <div className="bg-primary text-tertiary min-h-screen p-6">
       <div className="mx-auto max-w-4xl">
         <h2 className="mt-6 mb-2 flex items-center text-2xl">
-          1. Accessibility Place Holder
+          1. Accessibility Placeholder
           <MdAccessibilityNew size={ICON_SIZE} className="ml-3 text-tertiary" />
         </h2>
         <p>
-          This is placeholder text for accessibility and how it works place
-          information about the rating system clculations here
+          This is placeholder text for accessibility and how it works. Place
+          information about the rating system calculations here.
         </p>
       </div>
     </div>
