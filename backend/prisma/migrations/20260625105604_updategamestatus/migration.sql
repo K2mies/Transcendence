6be3e75a24e413700616d16ee218fdaca02d52b0
sonjasonjao <182800674+sonjasonjao@users.noreply.github.com>
2026-07-03 15:39:23 +0300

    GG38: fix: replaced previous updategamestatus migration with new resetgamestatus which doesn't overlap with add message read migration

--- backend/prisma/migrations/20260625105604_updategamestatus/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterEnum
-ALTER TYPE "GameStatus" ADD VALUE 'NONE';
-
--- CreateIndex
-CREATE INDEX "Message_senderId_receiverId_createdAt_idx" ON "Message"("senderId", "receiverId", "createdAt");
-
--- CreateIndex
-CREATE INDEX "Message_receiverId_senderId_createdAt_idx" ON "Message"("receiverId", "senderId", "createdAt");
