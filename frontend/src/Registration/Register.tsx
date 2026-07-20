cb21206659f94d9fd303da655beda21f22428ed1
Ross Hvidsten <Rosshvidsten@googlemail.com>
2026-07-20 12:22:22 +0300

    fixed some of the formatting of the registration forms in react

--- frontend/src/Registration/Register.tsx
+++ frontend/src/Registration/Register.tsx
@@ -4,8 +4,8 @@ import type { RegistrationProps } from "../types";
 
 function SignUp({ setMyCurrUser }: RegistrationProps) {
   return (
-    <div className="bg-secondary flex min-h-screen flex-col items-center justify-center text-center">
-      <div className="bg-tertiary p-4 rounded-lg">
+    <div className="bg-secondary flex min-h-screen flex-col items-center justify-center text-center p-6">
+      <div className="bg-tertiary rounded-lg p-4">
         <h2 className="bg-primary text-tertiary p-4 rounded-t-lg">
           Sign up to GoodPlays
         </h2>
